b0dc095fd83a2114dcc04fd11de7a1c408ea80c4
annalisa-gyanfosu <45610866+annalisa-gyanfosu@users.noreply.github.com>
2020-05-03 21:53:32 -0400

    Add files via upload
    Student registration form project

---
+++ registration-form.js
@@ -0,0 +1,8 @@
+function submit (){
+    alert("Your form has been successfully submitted");
+}
+
+function validateEmail(elementValue){      
+    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailPattern.test(elementValue); 
+  } 
